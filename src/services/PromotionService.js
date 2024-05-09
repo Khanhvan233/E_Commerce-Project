fd024384bae793beb8e7cb9837f05a27a45e4dc3
MaiHai999 <maihai.tt@gmail.com>
2024-05-09 16:58:02 +0700

    okla

--- src/services/PromotionService.js
+++ src/services/PromotionService.js
@@ -5,8 +5,28 @@ export const getPromotion = async () => {
     return response.data;
 }
 
-export const getProductPromotion = async (data) => {
-    const response = await axios.get(`${process.env.REACT_APP_API_URL}/promotion_get_id`, data);
-    return response.data;
+
+export function GetProductPromotion(data){
+    return axios.post(`${process.env.REACT_APP_API_URL}/promotion_get_id`,data);
 }
+
+
+export function GetAllProduct(){
+    return axios.get(`${process.env.REACT_APP_API_URL}/product_get`);
+}
+
+
+export function AddPromotion(data){
+    return axios.post(`${process.env.REACT_APP_API_URL}/promotion_add`, data);
+}
+
+
+export function DeletePromotion(data){
+    return axios.post(`${process.env.REACT_APP_API_URL}/promotion_del`, data);
+}
+
+export function UpdatePromotion(data){
+    return axios.post(`${process.env.REACT_APP_API_URL}/promotion_update`, data);
+}
+
 
